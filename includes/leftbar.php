023e1850038d3bb9318c0db2d34f71883dc23971
Kalpesh Kumar <sutharkalpesh101@gmail.com>
2023-07-30 19:17:41 +0530

    Project submited

---
+++ includes/leftbar.php
@@ -0,0 +1,62 @@
+<div class="left-sidebar bg-black-300 box-shadow ">
+                        <div class="sidebar-content">
+                            <div class="user-info closed">
+                                <img src="images/20220201_214908.jpg" alt="not found" class="img-circle profile-img">
+                                <h6 class="title">Kalpesh Kumar</h6>
+                                <small class="info">Admin</small>
+                            </div>
+                            <!-- /.user-info -->
+
+                            <div class="sidebar-nav">
+                                <ul class="side-nav color-gray">
+                                    <li class="nav-header">
+                                        <span class="">Main Category</span>
+                                    </li>
+                                    <li>
+                                        <a href="dashboard.php"><span>Dashboard</span> </a>
+                                     
+                                    </li>
+
+                                    <li class="nav-header">
+                                        <span class="">Appearance</span>
+                                    </li>
+                                    <li class="has-children">
+                                        <a href="#"><span>Student Classes</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-class.php"><span>Create Class</span></a></li>
+                                            <li><a href="manage-classes.php"><span>Manage Classes</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+  <li class="has-children">
+                                        <a href="#"><span>Subjects</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="create-subject.php"><span>Create Subject</span></a></li>
+                                            <li><a href="manage-subjects.php"><span>Manage Subjects</span></a></li>
+                                           <li><a href="add-subjectcombination.php"><span>Add Subject Combination </span></a></li>
+                                           <a href="manage-subjectcombination.php"><span>Manage Subject Combination </span></a></li>
+                                        </ul>
+                                    </li>
+   <li class="has-children">
+                                        <a href="#"><span>Students</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-students.php"><span>Add Students</span></a></li>
+                                            <li><a href="manage-students.php"><span>Manage Students</span></a></li>
+                                           
+                                        </ul>
+                                    </li>
+<li class="has-children">
+                                        <a href="#"><span>Result</span> <i class="fa fa-angle-right arrow"></i></a>
+                                        <ul class="child-nav">
+                                            <li><a href="add-result.php"><span>Add Result</span></a></li>
+                                            <li><a href="manage-results.php"><span>Manage Result</span></a></li>
+                                           
+                                        </ul>
+                                        <li><a href="change-password.php"><span> Admin Change Password</span></a></li>
+                                           
+                                    </li>
+                            </div>
+                            <!-- /.sidebar-nav -->
+                        </div>
+                        <!-- /.sidebar-content -->
+                    </div>
